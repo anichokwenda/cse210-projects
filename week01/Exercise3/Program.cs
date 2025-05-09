8bfcb5436a76c267f04352b528966e9cc3a8c121
anichokwenda <anichokwenda@gmail.com>
2025-05-09 13:03:24 +0200

    Finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = 6;
+
+        int guess = -1;
+        do
+        {
+            Console.Write("What is the magic number? ");
+            while (!int.TryParse(Console.ReadLine(), out guess) || guess < 1 || guess > 100)
+            {
+                Console.Write("Invalid input. Please enter anumber between 1 and 100: ");
+            }
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != magicNumber);
     }
 }
