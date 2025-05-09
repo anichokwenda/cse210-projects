62a5546b8e1c31eb6cabee3ff19af7d7ac411429
anichokwenda <anichokwenda@gmail.com>
2025-05-09 12:19:17 +0200

    Finished exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("A");
+
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+
     }
 }
