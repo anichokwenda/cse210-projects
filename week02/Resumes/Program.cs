542c3fb7718f9d8a7172162d5a7a84dea82d6f4d
anichokwenda <anichokwenda@gmail.com>
2025-05-14 12:47:33 +0200

    finished

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Create a new job instance
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        //Create a new resume instance
+        Resume myResume = new Resume("Anisa Chokwenda");
+
+        //Add jobs to the resume 
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
+
+        //Display the first job title
+        myResume.Display();
     }
 }
