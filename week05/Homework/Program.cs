e1ee20aa4e061e444b234ee9a4ae8592b0a10671
anichokwenda <anichokwenda@gmail.com>
2025-06-05 10:32:23 +0200

    done with homework assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test the Assignment class
+        Assignment assignment = new Assignment("Henry Peters", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        // Test the MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Angel Miles", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Test the WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Andrew Jones", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
+
